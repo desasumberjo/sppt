5ba300532a22f07122f6ac341013a4c543243ee3
iqbalmnaufal <iqbalmnaufal@gmail.com>
2021-07-08 13:56:36 +0700

    Menambahkan Gambar

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="container border rounded p-5" id="container-login">
-    <div>
-      <h1>Login</h1>
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Nama Pengguna</label>
-        <input v-on:keyup.enter="submit" type="text" class="form-control" id="exampleFormControlInput1" placeholder="namapengguna" v-model="username" />
-      </div>
-      <div class="mb-3">
-        <label for="inputPassword" class="form-label">Password</label>
-        <input v-on:keyup.enter="submit" type="password" class="form-control" id="inputPassword" placeholder="************" v-model="password" />
-      </div>
-      <div class="d-grid gap-2">
-        <button @click="submit" class="btn btn-primary" type="button">Masuk</button>
+  <div id="container-login" class="d-flex align-items-center justify-content-center">
+    <div id="wrap-login" class="d-flex align-items-center justify-content-center border rounded p-5 d-flex justify-content-around" style="background-color: #fff !important">
+      <img src="../assets/logo-kab-blitar.jpg" id="gambar" alt="logo" />
+      <div>
+        <h3 class="d-flex justify-content-center">Member Login</h3>
+        <div class="mt-4 mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Nama Pengguna</label>
+          <input v-on:keyup.enter="submit" type="text" class="form-control" id="exampleFormControlInput1" placeholder="namapengguna" v-model="username" />
+        </div>
+        <div class="mb-3">
+          <label for="inputPassword" class="form-label">Password</label>
+          <input v-on:keyup.enter="submit" type="password" class="form-control" id="inputPassword" placeholder="************" v-model="password" />
+        </div>
+        <div class="d-grid gap-2">
+          <button @click="submit" class="btn btn-primary" type="button">Masuk</button>
+        </div>
       </div>
     </div>
   </div>
@@ -47,9 +50,18 @@ export default {
 </script>
 
 <style scoped>
+#gambar {
+  display: inline-block;
+  height: 29%;
+  width: 29%;
+}
 #container-login {
   /* position: relative; */
-  margin-top: 7.5em;
+  height: 100vh;
+  background-color: #b0bec5 !important;
   /* do */
+}
+#wrap-login {
+  border-radius: 30px !important;
 }
 </style>
