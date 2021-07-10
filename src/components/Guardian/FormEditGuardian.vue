dec51ef2a732a035d69d53513882cfbdb7a3bf69
iqbalmnaufal <iqbalmnaufal@gmail.com>
2021-07-10 13:05:46 +0700

    Form Edit Pamong

--- src/components/Guardian/FormEditGuardian.vue
+++ src/components/Guardian/FormEditGuardian.vue
@@ -4,17 +4,20 @@
     <div class="row mt-4">
       <div class="col-12">
         <label class="form-label">ID</label>
-        <input v-on:keyup.enter="editGuardian" v-model="id" type="number" class="form-control" />
+        <input v-on:keyup.enter="sendGuardian" v-model="id" type="number" class="form-control" />
       </div>
       <div class="col-12 my-3">
         <label class="form-label">Nama</label>
-        <input v-on:keyup.enter="editGuardian" v-model="name" type="text" class="form-control" />
+        <input v-on:keyup.enter="sendGuardian" v-model="name" type="text" class="form-control" />
       </div>
+    </div>
+    <div class="alert alert-danger" v-if="alert" role="alert">
+      Masukkan Data yang Valid!
     </div>
 
     <div class="d-flex justify-content-between mt-4">
       <button @click="previous" class="btn btn-primary">Kembali</button>
-      <button @click="sendGuardian" class="btn btn-primary">Kirim</button>
+      <button @click="sendGuardian" class="btn btn-primary" :disabled="isLoading"><span v-if="isLoading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span v-if="!isLoading">Kirim</span></button>
     </div>
   </div>
 </template>
@@ -24,15 +27,30 @@ import axios from "axios";
 export default {
   data() {
     return {
+      alert: false,
+      isLoading: false,
       id: "",
       name: "",
     };
+  },
+  mounted: function() {
+    axios
+      .get("api/v1/guardian/" + sessionStorage.getItem("IDGuardian"), {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        this.id = response.data.data.id;
+        this.name = response.data.data.name;
+      });
   },
   methods: {
     previous() {
       this.$router.push({ name: "DataGuardian" });
     },
     sendGuardian() {
+      this.isLoading = true;
       axios
         .put(
           "api/v1/guardian/" + sessionStorage.getItem("IDGuardian"),
@@ -47,8 +65,13 @@ export default {
           }
         )
         .then(() => {
+          this.isLoading = false;
           this.$router.push({ name: "DataGuardian" });
           sessionStorage.removeItem("IDGuardian");
+        })
+        .catch(() => {
+          this.isLoading = false;
+          this.alert = true;
         });
     },
   },
