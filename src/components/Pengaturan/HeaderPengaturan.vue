b48e9cc12f9284e0095b8fd94a0e81e5d7595450
iqbalmnaufal <iqbalmnaufal@gmail.com>
2021-07-10 18:12:42 +0700

    Show Profile Picture

--- src/components/Pengaturan/HeaderPengaturan.vue
+++ src/components/Pengaturan/HeaderPengaturan.vue
@@ -3,28 +3,55 @@
   <div class="row">
     <div class="col-1">
       <img
-        src="../../assets/pp.jpg"
+        v-if="profilePicture != null"
+        :src="`${profilePicture}`"
         alt="profile picture"
         class="img-fluid rounded-circle my-4 p-1 d-none
         d-md-block shadow"
       />
+      <svg
+        v-if="profilePicture == null"
+        xmlns="http://www.w3.org/2000/svg"
+        width="60"
+        height="60"
+        fill="currentColor"
+        class="bi bi-person img-fluid rounded-circle my-4 p-1 d-none
+        d-md-block shadow"
+        viewBox="0 0 16 16"
+      >
+        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+      </svg>
     </div>
     <div class="col d-flex align-items-center mt-3">
       <p class="navbar-brand mx-0 font-weight text-nowrap">
-        {{ profile.name }} <br /><span class="fs-6" style="color: #7A7E86">{{ profile.occupation }}</span>
+        {{ name }} <br /><span class="fs-6" style="color: #7A7E86">{{ occupation }}</span>
       </p>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  props: ["profile"],
   data() {
     return {
       name: "",
       occupation: "",
+      profilePicture: "",
     };
+  },
+  mounted: function() {
+    axios
+      .get("api/v1/profile", {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        this.name = response.data.data.name;
+        this.occupation = response.data.data.occupation;
+        this.profilePicture = response.data.data.image_url;
+      });
   },
 };
 </script>
--- src/views/Pengaturan.vue
+++ src/views/Pengaturan.vue
@@ -4,8 +4,8 @@
       <Aside />
       <main class="col px-0 flex-grow-1 mx-5 my-5">
         <div class="conainer py-3">
-          <HeaderPengaturan :profile="profile" />
-          <BodyPengaturan :profile="profile" />
+          <HeaderPengaturan />
+          <BodyPengaturan />
         </div>
       </main>
     </div>
@@ -13,28 +13,11 @@
 </template>
 
 <script>
-import axios from "axios";
 import Aside from "../components/Aside.vue";
 import HeaderPengaturan from "../components/Pengaturan/HeaderPengaturan.vue";
 import BodyPengaturan from "../components/Pengaturan/BodyPengaturan.vue";
 export default {
   components: { Aside, HeaderPengaturan, BodyPengaturan },
   name: "Pengaturan",
-  data() {
-    return {
-      profile: "",
-    };
-  },
-  mounted: function() {
-    axios
-      .get("api/v1/profile", {
-        headers: {
-          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
-        },
-      })
-      .then((response) => {
-        this.profile = response.data.data;
-      });
-  },
 };
 </script>
