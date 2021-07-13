4c4a247c7a642b93e226fd20b8c949d5d4945a7f
iqbalmnaufal <iqbalmnaufal@gmail.com>
2021-07-13 16:42:42 +0700

    Data Display

--- src/components/Home/TableDisplay.vue
+++ src/components/Home/TableDisplay.vue
@@ -4,19 +4,34 @@
       <tr>
         <th>Nama</th>
         <th>NOP</th>
-        <th>ID Pamong</th>
         <th>Luas Lahan</th>
         <th>Luas Bangunan</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>Iqbal</td>
-        <td>123456789</td>
-        <td class="text-center">7</td>
-        <td class="text-center">1000 m<sup>2</sup></td>
-        <td class="text-center">1000 m<sup>2</sup></td>
+      <tr v-for="result in results" :key="result.id">
+        <td>{{ result.taxpayer.name }}</td>
+        <td>{{ result.tax_object.nop }}</td>
+        <td>{{ result.tax_object.land_area }} m<sup>2</sup></td>
+        <td>{{ result.tax_object.building_area }} m<sup>2</sup></td>
       </tr>
     </tbody>
   </table>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      results: "",
+    };
+  },
+  mounted: function() {
+    axios.get("/api/v1/sppts").then((response) => {
+      console.log(response.data.data.data);
+      this.results = response.data.data.data;
+    });
+  },
+};
+</script>
