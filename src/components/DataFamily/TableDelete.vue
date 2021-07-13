c8b937c0c82c740d8e10e0e362040735432a93bf
iqbalmnaufal <iqbalmnaufal@gmail.com>
2021-07-13 13:22:52 +0700

    PATCH & Delete Family

---
+++ src/components/DataFamily/TableDelete.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="row d-flex justify-content-between">
+    <div class="col d-flex justify-content-between my-3">
+      <h3>Pindah Data Family</h3>
+    </div>
+  </div>
+  <div class="my-2">
+    <label class="form-label">Pilih Keluarga</label>
+    <div class="dropdown d-grid gap-2">
+      <input class="form-control" list="listOptionsFamily" placeholder="Type to search..." v-model="familyID" />
+      <datalist id="listOptionsFamily">
+        <option v-for="family in families" :key="family.id">{{ family.id }} - {{ family.name }}</option>
+      </datalist>
+    </div>
+  </div>
+  <div class="alert alert-warning my-3" role="alert">
+    Sebelum menghapus keluarga, pastikan data yang terkait dipindahkan terlebih dahulu!
+  </div>
+  <table class="table my-4">
+    <thead class="table-secondary">
+      <tr>
+        <th>Nama</th>
+        <th>NOP</th>
+        <th>RT</th>
+        <th>RW</th>
+        <th>Jalan</th>
+        <th>Desa</th>
+        <th>Aksi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="result in result" :key="result.id">
+        <td>{{ result.taxpayer.name }}</td>
+        <td>{{ result.tax_object.nop }}</td>
+        <td>{{ result.tax_object.address.rt }}</td>
+        <td>{{ result.tax_object.address.rw }}</td>
+        <td>{{ result.tax_object.address.road }}</td>
+        <td>{{ result.tax_object.address.village }}</td>
+        <td>
+          <input type="checkbox" :value="`${result.id}`" v-model="checkedID" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <div class="d-flex justify-content-end mt-4">
+    <button @click="submit" class="btn btn-primary">Kirim</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      families: null,
+      result: "",
+      familyID: "",
+      selectedFamilyID: "",
+      checkedID: [],
+      data: [],
+    };
+  },
+  methods: {
+    submit() {
+      let regex = /\d+/g;
+      this.selectedFamilyID = this.familyID.match(regex);
+      for (let i = 0; i < this.checkedID.length; i++) {
+        this.data.push({
+          id: this.checkedID[i],
+          family_id: this.selectedFamilyID[0],
+        });
+      }
+      axios
+        .patch(
+          "api/v1/sppt/family-update",
+          {
+            sppt_objects: this.data,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+            },
+          }
+        )
+        .then(() => {
+          axios
+            .delete("api/v1/family/" + sessionStorage.getItem("idFamily"), {
+              headers: {
+                Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+              },
+            })
+            .then(() => {
+              sessionStorage.removeItem("idFamily");
+              this.$router.push({ name: "DataFamily" });
+            });
+        });
+    },
+  },
+  mounted: function() {
+    axios
+      .get("api/v1/sppt/family/" + sessionStorage.getItem("idFamily"), {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        this.result = response.data.data;
+      });
+    axios
+      .get("api/v1/families", {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        this.families = response.data.data;
+      });
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap");
+h3 {
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+}
+th {
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+}
+td {
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+}
+option {
+  border-radius: 10px !important;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,137 +1,145 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/Login.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Login',
-    component: Login
+    path: "/",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/pencarian',
-    name: 'Pencarian',
+    path: "/pencarian",
+    name: "Pencarian",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Pencarian.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/Pencarian.vue"),
   },
   {
-    path: '/tes',
-    name: 'Tes',
+    path: "/tes",
+    name: "Tes",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Tes.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/Tes.vue"),
   },
   {
-    path: '/tambah-data',
-    name: 'TambahData',
+    path: "/tambah-data",
+    name: "TambahData",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/TambahData.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/TambahData.vue"),
   },
   {
-    path: '/edit-data',
-    name: 'EditSPPT',
+    path: "/edit-data",
+    name: "EditSPPT",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/EditSPPT.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/EditSPPT.vue"),
   },
   {
-    path: '/mutasi-data',
-    name: 'MutasiDataPersonal',
+    path: "/mutasi-data",
+    name: "MutasiDataPersonal",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/MutasiDataPersonal.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/MutasiDataPersonal.vue"),
   },
   {
-    path: '/data-family',
-    name: 'DataFamily',
+    path: "/data-family",
+    name: "DataFamily",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/DataFamily.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/DataFamily.vue"),
   },
   {
-    path: '/data-admin',
-    name: 'DataAdmin',
+    path: "/data-admin",
+    name: "DataAdmin",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/DataAdmin.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/DataAdmin.vue"),
   },
   {
-    path: '/tambah-admin',
-    name: 'TambahAdmin',
+    path: "/tambah-admin",
+    name: "TambahAdmin",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/TambahAdmin.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/TambahAdmin.vue"),
   },
   {
-    path: '/tambah-keluarga',
-    name: 'AddFamily',
+    path: "/tambah-keluarga",
+    name: "AddFamily",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AddFamily.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/AddFamily.vue"),
   },
   {
-    path: '/edit-family',
-    name: 'EditFamily',
+    path: "/edit-family",
+    name: "EditFamily",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/EditFamily.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/EditFamily.vue"),
   },
   {
-    path: '/data-pamong',
-    name: 'DataGuardian',
+    path: "/delete-keluarga",
+    name: "DeleteFamily",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Guardian/DataGuardian.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/DeleteFamily.vue"),
   },
   {
-    path: '/tambah-pamong',
-    name: 'AddGuardian',
+    path: "/data-pamong",
+    name: "DataGuardian",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Guardian/AddGuardian.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/Guardian/DataGuardian.vue"),
   },
   {
-    path: '/edit-pamong',
-    name: 'EditGuardian',
+    path: "/tambah-pamong",
+    name: "AddGuardian",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Guardian/EditGuardian.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/Guardian/AddGuardian.vue"),
   },
   {
-    path: '/delete-pamong',
-    name: 'DeleteGuardian',
+    path: "/edit-pamong",
+    name: "EditGuardian",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Guardian/DeleteGuardian.vue')
+    component: () => import(/* webpackChunkName: "about" */ "../views/Guardian/EditGuardian.vue"),
   },
   {
-    path: '/pengaturan',
-    name: 'Pengaturan',
+    path: "/delete-pamong",
+    name: "DeleteGuardian",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Pengaturan.vue')
-  }
-]
+    component: () => import(/* webpackChunkName: "about" */ "../views/Guardian/DeleteGuardian.vue"),
+  },
+  {
+    path: "/pengaturan",
+    name: "Pengaturan",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/Pengaturan.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
