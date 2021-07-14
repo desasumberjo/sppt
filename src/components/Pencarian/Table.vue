1c58e4d4a148d768c760f5e83bfc6f535c09dd10
iqbalmnaufal <iqbalmnaufal@gmail.com>
2021-07-14 11:00:37 +0700

    Detail SPPT

--- src/components/Pencarian/Table.vue
+++ src/components/Pencarian/Table.vue
@@ -1,5 +1,5 @@
 <template>
-  <table class="table">
+  <table class="table table-hover">
     <thead class="table-secondary">
       <tr>
         <th>Nama</th>
@@ -11,7 +11,7 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="result in resultData" :key="result.id">
+      <tr @click="detail(result.tax_object.nop)" v-for="result in resultData" :key="result.id" data-bs-toggle="modal" data-bs-target="#exampleModal">
         <td>{{ result.taxpayer.name }}</td>
         <td>{{ result.tax_object.nop }}</td>
         <td class="text-center">{{ result.tax_object.guardian_id }}</td>
@@ -36,6 +36,26 @@
       </tr>
     </tbody>
   </table>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Detail</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Nama : {{ modalData.name }}</p>
+          <p>NOP : {{ modalData.nop }}</p>
+          <p>Jalan : {{ modalData.road }}</p>
+          <p>Desa : {{ modalData.village }}</p>
+          <p>RT / RW : {{ modalData.rt }} / {{ modalData.rw }}</p>
+          <p>Keluarga : {{ modalData.family }}</p>
+          <p>Luas Lahan : {{ modalData.landArea }} m<sup>2</sup></p>
+          <p>Luas Bangunan : {{ modalData.buildingArea }} m<sup>2</sup></p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -45,9 +65,35 @@ export default {
   data() {
     return {
       hasilPencarian: null,
+      id: "",
+      modalData: {
+        name: "",
+        nop: "",
+        road: "",
+        village: "",
+        rt: "",
+        rw: "",
+        family: "",
+        landArea: "",
+        buildingArea: "",
+      },
     };
   },
   methods: {
+    detail(id) {
+      this.id = id;
+      axios.get("api/v1/sppt/" + this.id).then((response) => {
+        this.modalData.name = response.data.data.taxpayer.name;
+        this.modalData.nop = response.data.data.tax_object.nop;
+        this.modalData.road = response.data.data.taxpayer.address.road;
+        this.modalData.village = response.data.data.taxpayer.address.village;
+        this.modalData.rt = response.data.data.taxpayer.address.rt;
+        this.modalData.rw = response.data.data.taxpayer.address.rw;
+        this.modalData.family = response.data.data.taxpayer.family.name;
+        this.modalData.landArea = response.data.data.tax_object.land_area;
+        this.modalData.buildingArea = response.data.data.tax_object.building_area;
+      });
+    },
     edit(result) {
       sessionStorage.setItem("data", JSON.stringify(result));
       this.$router.push({ name: "EditSPPT" });
@@ -80,6 +126,7 @@ th {
 td {
   font-family: "Poppins", sans-serif;
   font-weight: 400;
+  cursor: pointer;
 }
 svg {
   cursor: pointer;
